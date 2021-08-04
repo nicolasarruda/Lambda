48ea57cbd1c7b5e72706228e5ea0f021f6fb2de2
Nicolas <nlarruda@hotmail.com>
2021-08-04 12:30:21 -0300

    Completing and adding new programs

--- application/Program2.java
+++ application/Program2.java
@@ -1,22 +1,10 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import entities.Product;
-
 public class Program2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		List<Product> list = new ArrayList<>();
 
-		list.add(new Product("Tv", 900.00));
-		list.add(new Product("Notebook", 1200.00));
-		list.add(new Product("Tablet", 450.00));
-
-		list.sort(new MyComparator());	
 	}
 
 }
